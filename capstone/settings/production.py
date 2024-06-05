2dbd3892cd3c29609e47296583b0957b9894c01f
Kevin Velez <kevin.alejandro.velez@correounivalle.edu.co>
2024-06-05 16:01:14 -0500

    Update production.py

--- capstone/settings/production.py
+++ capstone/settings/production.py
@@ -2,6 +2,7 @@ import os
 from ..logging import *
 from .base import *
 from dotenv import load_dotenv
+from pathlib import Path
 
 # Cargar el archivo .env si existe
 dotenv_path = Path.joinpath(BASE_DIR, '.env')
@@ -10,9 +11,8 @@ load_dotenv(dotenv_path)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = [ '*' ]
-
-STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
+# Otras configuraciones
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
